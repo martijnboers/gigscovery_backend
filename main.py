c7cd5bf66e17ea4718b07da9046ea3b924f23212
mabergerx <maberger0811@gmail.com>
2020-02-24 17:50:50 +0100

    resolved merge conflict

--- main.py
+++ main.py
@@ -20,22 +20,7 @@ async def read_user(user_id: str):
     return {"user_id": 1508}
 
 
-class ModelName(str, Enum):
-    alexnet = "alexnet"
-    resnet = "resnet"
-    lenet = "lenet"
-
-
-@gigscovery_app.get("/model/{model_name}")
-async def get_model(model_name: ModelName):
-    if model_name == ModelName.alexnet:
-        return {"model_name": model_name, "message": "Deep Learning FTW!"}
-    if model_name.value == "lenet":
-        return {"model_name": model_name, "message": "LeCNN all the images"}
-    return {"model_name": model_name, "message": "Have some residuals"}
-
-
-
 @gigscovery_app.get("/top_artists/{number_of_artists}")
 async def get_top_artists(number_of_artists: int):
     return {"top_artists": give_top_artists(number_of_artists)}
+
