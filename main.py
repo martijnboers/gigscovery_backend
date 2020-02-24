ef15d6a7d1d3b8f5be7623f2f295f3c4c0a9fd5a
力静蕾 <44409394+curiousseikatsu@users.noreply.github.com>
2020-02-24 16:31:54 +0100

    test2

--- main.py
+++ main.py
@@ -4,3 +4,12 @@ gigscovery_app = FastAPI()
 @gigscovery_app.get("/")
 async def root():
     return {"message": " Gigscovery API"}
+
+@gigscovery_app.get("/users/me")
+async def read_user_me():
+    return {"user_id": "Natasha"}
+
+
+@gigscovery_app.get("/users/{user_id}")
+async def read_user(user_id: str):
+    return {"user_id": 1508}
