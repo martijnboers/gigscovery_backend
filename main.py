bf3ab50204c8454b36137121847f007cf3a28a9e
力静蕾 <44409394+curiousseikatsu@users.noreply.github.com>
2020-02-24 16:38:47 +0100

    test3

--- main.py
+++ main.py
@@ -13,3 +13,11 @@ async def read_user_me():
 @gigscovery_app.get("/users/{user_id}")
 async def read_user(user_id: str):
     return {"user_id": 1508}
+
+@gigscovery_app.get("/model/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name == ModelName.alexnet:
+        return {"model_name": model_name, "message": "We are awesome"}
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+        return {"model_name": model_name, "message": "Have some residuals"}
