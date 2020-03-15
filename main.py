014fc435ae6e22bf1ae32b335f5c00c66a0cbd49
mabergerx <maberger0811@gmail.com>
2020-03-15 22:59:46 +0100

    all origins

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ origins = [
 
 gigscovery_app.add_middleware(
     CORSMiddleware,
-    allow_origins=[origins],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
